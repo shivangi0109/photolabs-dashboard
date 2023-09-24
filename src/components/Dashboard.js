c9e76eadf58f717766b99d389a8c469a21d936e6
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-23 21:05:28 -0600

    Handling State - When we click on one of the four panels, we will switch to a larger view of the panel that was clicked

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,18 +29,28 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} onSelect={this.selectPanel} />
     ))
 
     return <main className={dashboardClasses}>{panels}</main>;
