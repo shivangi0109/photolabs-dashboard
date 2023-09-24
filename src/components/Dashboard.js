9d8411a83fa52de395589fc40710699bc246fe53
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-23 20:47:22 -0600

    Added Panel component where each panel will display details about our PhotoLabs Project

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza",
+  }
+];
 
 class Dashboard extends Component {
   state = {
@@ -15,7 +39,11 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = data.map(panel => (
+      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />
+    ))
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
