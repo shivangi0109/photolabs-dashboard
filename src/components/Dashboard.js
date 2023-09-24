b3cd3958c9518216199368e67eb1908bf011f379
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-23 23:11:31 -0600

    Binding This - Use Arrow Function in Render & also added the behaviour to go from the single panel view to the view with four panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -34,9 +34,9 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
+    this.setState(previousState => ({
+     focused: previousState.focused !== null ? null : id
+    }));
   }
 
   render() {
@@ -50,7 +50,7 @@ class Dashboard extends Component {
 
     const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
       .map(panel => (
-      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} onSelect={this.selectPanel} />
+      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} onSelect={event => this.selectPanel(panel.id)} />
     ))
 
     return <main className={dashboardClasses}>{panels}</main>;
